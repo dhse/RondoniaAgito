3923e5e9d52578c644d0b42629ac9373b6cc9dab
dhse <dhsenanet@gmail.com>
2013-04-08 00:04:27 -0400

    Dominio QuemSomos

---
+++ Dominio/QuemSomos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class QuemSomos
+    {
+        public int QuemSomosId { get; set; }
+        public String QuemSomosDescricao { get; set; }
+    }
+}
