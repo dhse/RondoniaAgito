e8b36f56826b5edd9757a0fba8c83acb2b5939ee
dhse <dhsenanet@gmail.com>
2013-04-10 20:49:02 -0400

    Erro Inesperado

--- Aplicacao/AdminAplicacao.cs
+++ Aplicacao/AdminAplicacao.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Dominio;
 using Repositorio;
 
+
 namespace Aplicacao
 {
     public class AdminAplicacao
--- GuiWeb/Areas/Painel/Controllers/AdminController.cs
+++ GuiWeb/Areas/Painel/Controllers/AdminController.cs
@@ -4,7 +4,6 @@ using Dominio;
 
 namespace GuiWeb.Areas.Painel.Controllers
 {
-    [Authorize]
     public class AdminController : Controller
     {
         //
